39de6930854302cf7d95048aab944fa8ce0f65d5
SG-mancer <fxs696226@gmail.com>
2023-12-07 09:50:53 +1100

    basic forms function

--- site/new_cost.php
+++ site/new_cost.php
@@ -6,6 +6,45 @@ if(!isset($_SESSION["access"])){
     header("Location: access/login.php");
 }else {
     //show page of login valid
+    try{       
+        //create a connection database and check if there was an error
+        require_once('../dbconfig.php');
+        if(!$db = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)) {
+            throw new Exception("Connection failed: ".mysql_connect_error());
+        }
+    } catch (Exception $e) {
+        // Log error, and then give link to login page for re-attempt
+        error_log("Connection Failed: " . $e->getMessage());
+        print("<h1>Connection Failed:</h1> <p>There was an issue with the connection. <a href=\"login.php\">Please try again</a></p>");
+    }
+
+    //Process form submission
+    if (isset($_POST['action']) && $_POST['action'] == "Add Cost") {
+        // Get data from form and session
+        $clodged = date("Y-m-d H:i:s", date_timestamp_get(date_create()));
+        $cdate = $_POST['date'];
+        $cuser = $_SESSION["account_no"];
+        $cammount = $_POST['ammount'];
+        $cnote = $_POST['note'];
+        $cimage = basename($_FILES['image']["name"]); //TODO handle file uploads
+
+        // SQL query to add the data
+        $updateSQL = "INSERT INTO costs (lodged, date, account_no, ammount, note, image) VALUES (
+            '$clodged', 
+            '$cdate',
+            '$cuser',
+            '$cammount',
+            '$cnote',
+            '$cimage')";
+  
+            //Add the new record
+            if (mysqli_query($db, $updateSQL)) {
+                print("cost added");
+            } else {
+                print("error... try again");
+            }
+    }
+
 ?>
 
     <!DOCTYPE html>
@@ -16,7 +55,7 @@ if(!isset($_SESSION["access"])){
         <title>Sydnicate Share - new Cost</title>
     </head>
     <body>
-        <form method="post">
+        <form action="#?nav=new" enctype="multipart/form-data" method="post">
             <label for="date">Date paid: </label>
             <input id="date" name="date" type="date" required>
 
@@ -29,7 +68,13 @@ if(!isset($_SESSION["access"])){
             <label for="image">Copy of reciept: </label>
             <input id="image" name="image" type="file">
 
+            <input type="submit" name="action" value="Add Cost">
         </form>
+
+        <footer> 
+            <a href="index.php">index</a> 
+        </footer>
+
     </body>
     </html>
 
--- site/new_maintenance_req.php
+++ site/new_maintenance_req.php
@@ -6,6 +6,42 @@ if(!isset($_SESSION["access"])){
     header("Location: access/login.php");
 }else {
     //show page of login valid
+    try{       
+        //create a connection database and check if there was an error
+        require_once('../dbconfig.php');
+        if(!$db = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)) {
+            throw new Exception("Connection failed: ".mysql_connect_error());
+        }
+    } catch (Exception $e) {
+        // Log error, and then give link to login page for re-attempt
+        error_log("Connection Failed: " . $e->getMessage());
+        print("<h1>Connection Failed:</h1> <p>There was an issue with the connection. <a href=\"login.php\">Please try again</a></p>");
+    }
+
+    //Process form submission
+    if (isset($_POST['action']) && $_POST['action'] == "Add Request") {
+        // Get data from form and session
+        $ruser = $_SESSION["account_no"];
+        $rsafety = !empty($_POST['safety']);
+        $rtitle = $_POST['title'];
+        $rdescription = $_POST['note'];
+        $rimage = basename($_FILES['image']["name"]); //TODO handle file uploads
+
+        // SQL query to add the data
+        $updateSQL = "INSERT INTO maintenance_request (account_no, safety_issue, title, description, image) VALUES (
+            '$ruser',
+            '$rsafety',
+            '$rtitle',
+            '$rdescription',
+            '$rimage')";
+    
+            //Add the new record
+            if (mysqli_query($db, $updateSQL)) {
+                print("maintenance request added");
+            } else {
+                print("error... try again");
+            }
+    }
 ?>
 
     <!DOCTYPE html>
@@ -16,7 +52,10 @@ if(!isset($_SESSION["access"])){
         <title>Syndicate Share - new Maintenance Request</title>
     </head>
     <body>
-        <form>
+        <form action="#?nav=new" enctype="multipart/form-data" method="post">
+
+            <label for="title">Title of Maintenance Request: </label>
+            <input id="title" name="title" type="text" required>
 
             <label for="note">Description: </label>
             <textarea id="note" name="note" rows="3" cols="30" placeholder="description of spend" required></textarea>
@@ -24,9 +63,15 @@ if(!isset($_SESSION["access"])){
             <label for="safety">This is a safety issue, that need to be reviewed before next usage</label>
             <input id="safety" name="safety" type="checkbox">
 
-            <label for="image">Copy of reciept: </label>
+            <label for="image">Image of issue: </label>
             <input id="image" name="image" type="file">
+
+            <input type="submit" name="action" value="Add Request">
         </form>
+
+        <footer> 
+            <a href="index.php">index</a> 
+        </footer>
         
     </body>
     </html>
